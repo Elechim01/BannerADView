9578004e0a0ba74f78d641456163f6a66019c362
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2021-11-21 21:19:51 +0100

    Initial Commit

---
+++ Shared/BannerADViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  BannerADViewApp.swift
+//  Shared
+//
+//  Created by Michele Manniello on 21/11/21.
+//
+
+import SwiftUI
+
+@main
+struct BannerADViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
